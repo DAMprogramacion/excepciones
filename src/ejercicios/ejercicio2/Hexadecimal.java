20e4536213cdb7e9b059d3c11a8a09066ecc9b19
manuel <mmolmil158@g.educaand.es>
2024-03-07 13:11:04 +0100

    Fin del tema

--- src/ejercicios/ejercicio2/Hexadecimal.java
+++ src/ejercicios/ejercicio2/Hexadecimal.java
@@ -12,10 +12,21 @@ public class Hexadecimal implements Numero {
 
     @Override
     public int valorDecimal() {
-        return Integer.parseInt(valorHexadecimal, 16);
+        double suma = 0;
+        StringBuilder builder = new StringBuilder(valorHexadecimal);
+        String valorReverse = builder.reverse().toString(); //011
+        for (int i = 0; i < valorReverse.length(); i++) {
+            char caracter = valorReverse.toLowerCase().charAt(i); // 0 1 1
+            if (Character.isDigit(caracter))
+                suma += Math.pow(16, i) * (caracter - 48);
+            else
+                suma += Math.pow(16, i) * (caracter - 87);
+        }
+        return (int) suma;
+         //return Integer.parseInt(valorHexadecimal, 16);
     }
 
-    /*public static void main(String[] args) {
-        System.out.println(new Hexadecimal("1f").valorDecimal());
-    }*/
+    public static void main(String[] args) throws FormatoException {
+        System.out.println(new Hexadecimal("10").valorDecimal());
+    }
 }
